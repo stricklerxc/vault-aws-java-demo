ea9ebd08ba7ac0cc53e2092f86c6edf9774635d6
stricklerxc <29963548+stricklerxc@users.noreply.github.com>
2021-04-19 12:10:51 -0400

    initial commit

---
+++ src/main/java/com/github/stricklerxc/awsvaultenginedemo/AwsVaultEngineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.stricklerxc.awsvaultenginedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsVaultEngineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsVaultEngineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/stricklerxc/awsvaultenginedemo/AwsVaultEngineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stricklerxc.awsvaultenginedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsVaultEngineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
